8997b53e583dee19a9b35b95021e9025af006495
r3xyzz <rexgamer152@gmail.com>
2025-04-21 21:58:42 -0400

    ahora si commit de esqueleto con carpeta separadas

--- PROYECTO-CONTABILIDAD/miapp/miapp/asgi.py
+++ PROYECTO-CONTABILIDAD/miapp/miapp/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for AppContabilidad project.
+ASGI config for miapp project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AppContabilidad.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'miapp.settings')
 
 application = get_asgi_application()
--- PROYECTO-CONTABILIDAD/miapp/miapp/urls.py
+++ PROYECTO-CONTABILIDAD/miapp/miapp/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for AppContabilidad project.
+URL configuration for miapp project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
