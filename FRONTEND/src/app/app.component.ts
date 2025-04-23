ee1593c9157ace829a31aaf6ed361787804133b4
aravegac <ara.vegac@duocuc.cl>
2025-04-22 20:20:49 -0400

    Sincronización de django a angular EXITOSA por medio de API

--- FRONTEND/src/app/app.component.ts
+++ FRONTEND/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,15 @@ import { Component } from '@angular/core';
   standalone: false,
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'FRONTEND';
+export class AppComponent implements OnInit{
+
+  message = '';
+
+  constructor(private ApiService: ApiService) {}
+
+  ngOnInit() {
+    this.ApiService.getMessage().subscribe((data: any) => {
+      this.message = data.message;
+    });
+  }
 }
