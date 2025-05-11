829bf97dae8a9c70a37d17082f3979ce3f196f51
r3xyzz <rexgamer152@gmai.com>
2025-05-11 15:33:23 -0400

    correccion de rutas y agregado de componentes para la visualizcion de tablas o info de la BD sql aws

--- FRONTEND/src/app/app-routing.module.ts
+++ FRONTEND/src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ReportComponent } from './reports/report.component'; // <-- Corrección aquí
 
 const routes: Routes = [];
 
