0b73eb8a4962cb805c3f2aa62452b631cb9f71c4
r3xyzz <rexgamer152@gmai.com>
2025-05-13 19:12:18 -0400

    COMENTARIO SUPER IMPORTANTE

--- BACKEND/moduloContabilidad/models.py
+++ BACKEND/moduloContabilidad/models.py
@@ -1,6 +1,18 @@
 from django.db import models
 
 # Create your models here.
+# Definimos los modelos de la base de datos para la contabilidad
+# Estos modelos representan las tablas de la base de datos
+# y se utilizan para interactuar con la base de datos a través de Django ORM
+# Importamos el módulo models de Django para definir nuestros modelos
+# Definimos la clase CuentaContable que representa la tabla CuentaContable en la base de datos
+# Esta clase hereda de models.Model, lo que significa que es un modelo de Django
+# La clase Meta define la configuración de la tabla en la base de datos
+
+
+# TENER OJO CON LAS MAYUSCULAS Y MINUSCULAS
+# Asegurarse de que los nombres de las tablas y columnas coincidan con los de la base de datos
+
 class CuentaContable(models.Model):
     IdCuenta = models.AutoField(primary_key=True)  # Especificamos que IdCuenta es la clave primaria
     nombreCuenta = models.CharField(db_column='nombreCuenta', max_length=500)  # Field name made lowercase.
