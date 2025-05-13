8c0a7be069fdda91504fe5ab495d048f760c2892
r3xyzz <rexgamer152@gmai.com>
2025-05-13 19:10:47 -0400

    cambios en el settings, models y serializers de django. Conexion y sincronizacion de la base de datos de Django con AWS

--- BACKEND/moduloContabilidad/models.py
+++ BACKEND/moduloContabilidad/models.py
@@ -2,24 +2,44 @@ from django.db import models
 
 # Create your models here.
 class CuentaContable(models.Model):
-    nombreCuenta = models.CharField(max_length=500)
-    tipoCuenta = models.CharField(max_length=500)
-    codigoCuenta = models.CharField(max_length=600)
-    descripcionCuenta = models.TextField()
+    IdCuenta = models.AutoField(primary_key=True)  # Especificamos que IdCuenta es la clave primaria
+    nombreCuenta = models.CharField(db_column='nombreCuenta', max_length=500)  # Field name made lowercase.
+    tipoCuenta = models.CharField(db_column='tipoCuenta', max_length=500)  # Field name made lowercase.
+    codigoCuenta = models.CharField(db_column='codigoCuenta', max_length=600)  # Field name made lowercase.
+    descripcionCuenta = models.TextField(db_column='descripcionCuenta')  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'CuentaContable'
 
 class AsientoContable(models.Model):
-    fechaAsiento = models.DateField()
-    descripcionAsiento = models.TextField()
-    referenciaAsiento = models.CharField(max_length=500)
+    IdAsiento = models.AutoField(primary_key=True)  # Especificamos que IdCuenta es la clave primaria
+    fechaAsiento = models.DateField(db_column='fechaAsiento')  # Field name made lowercase.
+    descripcionAsiento = models.TextField(db_column='descripcionAsiento')  # Field name made lowercase.
+    referenciaAsiento = models.CharField(db_column='referenciaAsiento', max_length=500)  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'AsientoContable'
 
 class DetalleAsiento(models.Model):
-    idAsiento = models.ForeignKey(AsientoContable, on_delete=models.CASCADE)
-    idCuenta = models.ForeignKey(CuentaContable, on_delete=models.CASCADE)
-    debe = models.DecimalField(max_digits=10, decimal_places=2)
-    haber = models.DecimalField(max_digits=10, decimal_places=2)
+    IdDetalle = models.AutoField(primary_key=True)  # Especificamos que IdCuenta es la clave primaria
+    debe = models.DecimalField(max_digits=10, decimal_places=5)  # max_digits and decimal_places have been guessed, as this database handles decimal fields as float
+    haber = models.DecimalField(max_digits=10, decimal_places=5)  # max_digits and decimal_places have been guessed, as this database handles decimal fields as float
+    idAsiento = models.ForeignKey('Asientocontable', on_delete=models.CASCADE, db_column='idAsiento_id')  # Field name made lowercase.
+    idCuenta = models.ForeignKey('Cuentacontable', on_delete=models.CASCADE, db_column='idCuenta_id')  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'DetalleAsiento'
 
 class Transaccion(models.Model):
-    idAsiento = models.ForeignKey(AsientoContable, on_delete=models.CASCADE)
-    tipoTransaccion = models.CharField(max_length=500)
-    montoTransaccion = models.DecimalField(max_digits=10, decimal_places=2)
-    fechaTransaccion = models.DateField()
+    IdTransaccion = models.AutoField(primary_key=True)  # Especificamos que IdCuenta es la clave primaria
+    tipoTransaccion = models.CharField(db_column='tipoTransaccion', max_length=500)  # Field name made lowercase.
+    montoTransaccion = models.DecimalField(db_column='montoTransaccion', max_digits=10, decimal_places=5)  # Field name made lowercase. max_digits and decimal_places have been guessed, as this database handles decimal fields as float
+    fechaTransaccion = models.DateField(db_column='fechaTransaccion')  # Field name made lowercase.
+    idAsiento = models.ForeignKey('Asientocontable', on_delete=models.CASCADE, db_column='idAsiento_id')  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'Transaccion'
--- BACKEND/moduloContabilidad/serializers.py
+++ BACKEND/moduloContabilidad/serializers.py
@@ -4,19 +4,19 @@ from .models import CuentaContable, AsientoContable, DetalleAsiento, Transaccion
 class CuentaContableSerializer(serializers.ModelSerializer):
     class Meta:
         model = CuentaContable
-        fields = ('id', 'nombreCuenta', 'tipoCuenta', 'codigoCuenta', 'descripcionCuenta')
+        fields = ('nombreCuenta', 'tipoCuenta', 'codigoCuenta', 'descripcionCuenta')
 
 class AsientoContableSerializer(serializers.ModelSerializer):
     class Meta:
         model = AsientoContable
-        fields = ('id', 'fechaAsiento', 'descripcionAsiento', 'referenciaAsiento')
+        fields = ('fechaAsiento', 'descripcionAsiento', 'referenciaAsiento')
 
 class DetalleAsientoSerializer(serializers.ModelSerializer):
     class Meta:
         model = DetalleAsiento
-        fields = ('id', 'idAsiento', 'idCuenta', 'debe', 'haber')
+        fields = ('idAsiento', 'idCuenta', 'debe', 'haber')
 
 class TransaccionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Transaccion
-        fields = ('id', 'idAsiento', 'tipoTransaccion', 'montoTransaccion', 'fechaTransaccion')
+        fields = ('idAsiento', 'tipoTransaccion', 'montoTransaccion', 'fechaTransaccion')
