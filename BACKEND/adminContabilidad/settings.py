b0384a5e5092366574302547b73ae69eb3ba5636
r3xyzz <rexgamer152@gmai.com>
2025-05-18 23:21:44 -0400

    como ver el archivo cuando se envia de generar reporte

--- BACKEND/adminContabilidad/settings.py
+++ BACKEND/adminContabilidad/settings.py
@@ -10,6 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
+
+
+
 import pymysql
 pymysql.install_as_MySQLdb()
 
@@ -17,6 +21,8 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Quick-start development settings - unsuitable for production
