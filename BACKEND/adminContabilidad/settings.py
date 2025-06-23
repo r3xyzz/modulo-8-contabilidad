d531e089675e40412e2d150213dfc9d18922cd0f
Felipe Andaur <f.andaur@duocuc.cl>
2025-06-23 03:52:50 -0400

    Allowed_Host actualizado en settings.py oara despliegue de Ec2

--- BACKEND/adminContabilidad/settings.py
+++ BACKEND/adminContabilidad/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DJANGO_DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['34.225.192.85', 'localhost' , '127.0.0.1']
+ALLOWED_HOSTS = ['44.206.36.40', 'localhost', '127.0.0.1', '34.225.192.85']
 
 
 
